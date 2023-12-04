68f4aa5efba660ace826041ef192870b95a7fc94
Freddy0369 <152880549+Freddy0369@users.noreply.github.com>
2023-12-04 15:20:27 +0000

    Task1q1

---
+++ topic1/topic1t1.py
@@ -0,0 +1,14 @@
+array = [5,4,7,8,2,9]
+total = 0
+
+for i in range(len(array)):
+    print(array[len(array) - 1 - i])
+    total += array[i]
+
+print("Total: " + str(total))
+average = total / len(array)
+print("Average: " + str(average))
+
+#ChatGPT
+
+
