113e8f609f73fb7ec0141e81f11fc2fee7db106f
Freddy0369 <152880549+Freddy0369@users.noreply.github.com>
2024-01-16 10:10:29 +0000

    yeah

--- Misc/queue.py
+++ Misc/queue.py
@@ -19,14 +19,41 @@ def en_queue(new):
     size = size + 1
 
 def de_queue():
+    global size, max_size, front, rear
+
+    if is_empty() == True:
+        return "Queue is empty!"
+
     front += 1
-    rear -= 1
-    return queue[rear]
+    if rear == max_size - 1:
+        rear = 0
+        return queue[max_size - 1]
+    else:
+        rear += 1
+        return queue[rear - 1]
+
+
+def is_full():
+    if rear == max_size - 1:
+        return True
+    else:
+        return False
+    
+def is_empty():
+    if rear == -1:
+        return True
+    else:
+        return False
+    
 
 
 
 en_queue("spongebob")
 en_queue("spongebob")
+print(de_queue())
 en_queue("q")
+print(queue)
 en_queue("w")
+en_queue("spongebob")
+print(de_queue())
 print(queue)
