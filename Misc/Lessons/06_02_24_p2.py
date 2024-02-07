4887967d45849a9964b02f6b9312c86e5560c80b
Freddy0369 <152880549+Freddy0369@users.noreply.github.com>
2024-02-07 10:10:57 +0000

    yes

--- Misc/Lessons/06_02_24_p2.py
+++ Misc/Lessons/06_02_24_p2.py
@@ -1,11 +1,13 @@
 cur_pointer = 0
-array = [["Jake",2],[None, None],["Sam",3],["Bob",1],[None, None]]
+maximum = 0
+array = [["Jake",2],[None, None],["Sam",3],["Bob",5],[None, None], ["Mike",1]]
 
 def traverse(array_):
     if len(array_) == 0:
         return
     
     global cur_pointer
+    cur_pointer = 0
     
     while True:
         if array_[cur_pointer][1] == None:
@@ -14,8 +16,30 @@ def traverse(array_):
             print(array_[cur_pointer][0])
             cur_pointer = array_[cur_pointer][1]
 
+def get_last_item_index(array_):
+    if len(array_) == 0:
+        return
+    
+    global cur_pointer
+    cur_pointer = 0
+    
+    while True:
+        if array_[cur_pointer][1] == None:
+            return cur_pointer
+            break
+        else:
+            cur_pointer = array_[cur_pointer][1]
+
 def add_item(array_, data):
-    #!
+    #Traverse and find last item
+    get_last_item_index(array_)
+
+    #Set data
+    array_[cur_pointer][0] = data
+    #Locate a null value
+    for i in range(len(array_)):
+        if array_[i][1] == None:
+            array_[][]
     pass
 
 
